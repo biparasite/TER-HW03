b5db070d699b3c3c147ae3d268bc510391769de4
biparasite <biparasite@gmail.com>
2025-05-18 13:09:56 +0300

    add  TER-HW03_4.1

--- providers.tf
+++ providers.tf
@@ -2,9 +2,11 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+    }    
+    local = {
+      source  = "hashicorp/local"
     }
   }
-  required_version = "~>1.11"
 }
 
 provider "yandex" {
